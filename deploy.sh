b4dc045421f6c1c9f93272d6d50d5e75b4650509
chen08209 <chen08209@gmail.com>
2022-11-04 16:08:44 +0800

    actions

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@gitee.com:chen08209/follow.git master:gh-pages
+git push -f git@github.com:chen08209/follow.git main:gh-pages
 
 cd -
